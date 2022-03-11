123ed31d14bff6a06c3bbba02a3ee818cb17a81b
cjcapone85 <99698474+cjcapone85@users.noreply.github.com>
2022-03-10 19:39:52 -0600

    Class 1

---
+++ 1-1.js
@@ -0,0 +1,25 @@
+var h1Elements = document.getElementsByTagName("h1");
+
+for(var i = 0; i < h1Elements.length; i++) {
+   h1Elements[i].style.color = "blue";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// **Aim**: Create a new project (with only an HTML file) that uses JavaScript to change the color of a Heading element.
+
+// - [ ] Create new folder with HTML file
+// - [ ] Add a Heading Element with text inside
+// - [ ] Create a `<script></script>` tag at the bottom of the page
+// - [ ] Write the logic to select a heading and change its color
---
+++ 1-2.js
@@ -0,0 +1,7 @@
+
+
+
+
+
+
+// **Aim**: Create a new project (with an HTML and JavaScript) that uses JavaScript to change the color of a Heading element on a button click.
